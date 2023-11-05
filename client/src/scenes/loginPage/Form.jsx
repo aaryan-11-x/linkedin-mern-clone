3e7ec4e821fc0a4ab4120267c986e6b800c80dcc
Aaryan Golatkar <102362952+aaryan-11-x@users.noreply.github.com>
2023-11-05 10:38:58 +0530

    Update Form.jsx

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -77,7 +77,7 @@ const Form = () => {
     formData.append("picturePath", values.picture.name);
 
     const savedUserResponse = await fetch(
-      "http://localhost:3001/auth/register",
+      "https://linkedin-clone-server.vercel.app/auth/register",
       {
         method: "POST",
         body: formData,
@@ -92,7 +92,7 @@ const Form = () => {
   };
 
   const login = async (values, onSubmitProps) => {
-      const loggedInResponse = await fetch("http://localhost:3001/auth/login", {
+      const loggedInResponse = await fetch("https://linkedin-clone-server.vercel.app/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(values),
